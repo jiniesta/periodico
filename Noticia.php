f660946c1a8f7267457a0a82eca66124da37fb07
Javi <javi@javi-VGN-CS11S-W.(none)>
2013-04-08 13:56:53 +0200

    Primera prueba

---
+++ Noticia.php
@@ -0,0 +1,23 @@
+<?php
+Class Noticia{
+	private $titulo;
+	private $texto;
+	private $seccion;
+	public function get_titulo(){
+		return $this->titulo;
+	}
+	public function get_texto(){
+		return $this->texto;
+	}
+	public function set_titulo($titulo){
+		$this->titulo=$titulo;
+	}
+	public function set_texto($texto){
+		$this->titulo=$texto;
+	}
+	public function Noticia($titulo,$texto){
+		$this->titulo=$titulo;
+		$this->texto=$texto;
+	}
+}
+?>
---
+++ prueba.php
@@ -0,0 +1,12 @@
+<?php
+  error_reporting(E_ALL);
+  ini_set("display_errors", 1);
+  include "Noticia.php";
+  $noticia1=new Noticia("Lorenzo gana", "Rossi no pudo con el vigente campeón en Qatar");
+  echo "<h1>".$noticia1->get_titulo()."</h1>";
+  echo "<p>".$noticia1->get_texto()."</p>";
+  $noticia1->set_titulo("Baloncesto");
+  $noticia1->set_texto("El Fuenlabrada perderá la categoría para el año que viene.");
+  echo "<h1>".$noticia1->get_titulo()."</h1>";
+  echo "<p>".$noticia1->get_texto()."</p>";
+?>
