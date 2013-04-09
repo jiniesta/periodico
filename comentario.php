95bcde87e2dbdf500b9b46a1825bc4b4b2c7b1ca
Roberto <roberto.moraiscv@gmail.com>
2013-04-09 08:50:01 +0200

    segunda subida

---
+++ comentario.php
@@ -0,0 +1,32 @@
+<?php
+Class comentario{
+	private $usuario;
+	private $texto;
+	private $fecha;
+	public function get_usuario(){
+		return $this->usuario;
+	}
+	public function get_texto(){
+		return $this->texto;
+	}
+	
+	public function get_fecha(){
+		return $this->fecha;
+	}
+	public function set_usuario($usuario){
+		$this->titulo=$usuario;
+	}
+	public function set_texto($texto){
+		$this->titulo=$texto;
+	}
+	
+	public function set_fecha($usuario){
+		$this->fecha=$usuario;
+	}
+	public function Noticia($usuario,$texto,$fecha){
+		$this->titulo=$usuario;
+		$this->texto=$texto;
+		$this->fecha=$fecha;
+	}
+}
+?>
